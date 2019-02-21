28c10ddb00685f0f9ea22b502e6dbf1515af98d6
pawelNobis <pawel.nobis1@gmail.com>
2019-02-21 16:00:11 +0100

    refactoring

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer>
-            <h3><a href='http://frontepic.com' target='_blank'><small>front</small>Epic</a> &copy; 2k19</h3>
+            <h3><a href='http://frontepic.com' target='_blank' rel="noopener noreferrer" title='frontEpic.com'><small>front</small>Epic</a> &copy; 2k19</h3>
         </footer>
     );
 }
