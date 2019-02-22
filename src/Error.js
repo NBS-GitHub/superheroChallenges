d08ab00019d844a0e897fa2bcbc7ddc004b4e1ec
pawelNobis <pawel.nobis1@gmail.com>
2019-02-22 05:45:57 +0100

    error page

--- src/Error.js
+++ src/Error.js
@@ -5,7 +5,7 @@ const Error = () => {
     return (
         <div className='error'>
             <h3>The Jamerikan - Wrong Path</h3>
-            <iframe src="https://www.youtube.com/embed/94wFAzwO2oI">
+            <iframe src="https://www.youtube.com/embed/94wFAzwO2oI" title='The Jamerikan - Wrong Path'>
             </iframe>
         </div>
     );
